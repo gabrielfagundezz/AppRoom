1e1810f359388d8099745a7d8d0cf868f1aa607a
gabrielfagundezz <113125559+gabrielfagundezz@users.noreply.github.com>
2024-11-26 14:06:51 -0300

    ajuste DAO

--- app/src/main/java/com/example/inventory/data/ItemDao.kt
+++ app/src/main/java/com/example/inventory/data/ItemDao.kt
@@ -13,16 +13,12 @@ interface ItemDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(item: Item)
-
     @Update
     suspend fun update(item: Item)
-
     @Delete
     suspend fun delete(item: Item)
-
     @Query("SELECT * from items WHERE id = :id")
     fun getItem(id: Int): Flow<Item>
-
     @Query("SELECT * from items ORDER BY name ASC")
     fun getAllItems(): Flow<List<Item>>
 
